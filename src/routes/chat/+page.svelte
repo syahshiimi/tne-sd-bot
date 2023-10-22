4f386f254a32e698a58c05c132c418369a26112e
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2023-10-22 23:29:34 +0800

    init: tne-sd-bot backend

---
+++ src/routes/chat/+page.svelte
@@ -0,0 +1,26 @@
+<h1>StableHorde AI Bot Chat Feature</h1>
+
+<form class="form">
+	<div class="form-input">
+		<label for="prompt">Input your prompt: </label>
+		<textarea id="input-textarea" name="prompt" rows="10" cols="33" />
+	</div>
+	<button class="form-submit btn" type="submit"> Submit </button>
+</form>
+
+<style>
+	.form-input {
+		display: flex;
+		flex-direction: column;
+		row-gap: 1rem;
+	}
+
+	.form-submit {
+		margin-top: 1.5rem;
+		font-size: 1.25rem;
+	}
+
+	.form-input > label {
+		font-size: 1.5rem;
+	}
+</style>
